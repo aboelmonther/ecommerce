4419d109c6b6bacf352db171d4320f45d31775b7
aboelmonther <fawaz.blog@gmail.com>
2018-09-03 17:02:39 +0100

    first change

--- members.php
+++ members.php
@@ -16,44 +16,61 @@ if (isset($_SESSION['username'])){
    $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
    // Start Manage Page
+
    if ($do == 'Manage'){
 
    	// Manage Page
 
    }elseif ($do == 'Edit') {// Edit Page
 
-    //Data protection
+      //Data protection
       // if (isset($_GET['userid']) && is_numeric($_GET['userid'])) {
       // echo intval($_GET['userid']);  }else{ echo 0;}
 
 
-//short cut code Data protection
+       //short cut code Data protection
+      // check if get request userid is numeric & get the integer value of it
                                                                        //true                false
       $userid = isset($_GET['userid']) && is_numeric($_GET['userid']) ? intval($_GET['userid']) : 0;
 
        // Chek If The User Exist In Database
+      // select all data depend on this id 
 
     $stmt = $con->prepare("SELECT  * FROM users WHERE UserID = ? LIMIT 1 ");
 
+       // execute Query
+
        $stmt->execute(array($userid));
+
+       // fetch the data
+
        $row = $stmt->fetch();
+       //  the row count
        $count = $stmt->rowCount();
 
-        if ( $count = $stmt->rowCount() > 0 ){    ?>
+       // if ther's such id show the form
+
+        if (  $stmt->rowCount() > 0 ){    ?>
 
              <h1 class="text-center">Edit Member</h1>
 
              <div class="container">
-                <form class="form-horizontal">
+                <form class="form-horizontal" action="?do=Update" method="POST">
+                  <input type="hidden" name="userid" value="<?php echo $userid ?>">
+
                   <!-- Start username field-->
+
                   <div class="form-group form-group-lg">
                     <label class="col-sm-2 control-label">Username</label>
-                    <div class="col-sm-10 col-md-6">
-                      <input type="text" name="username" class="form-control" value="<?php echo $row[]?>" autocomplete="off" />
+                    <div class="col-sm-10 col-md-6"> 
+                      <input type="text" name="username" class="form-control" value="<?php echo $row['username']?>" autocomplete="off" />
                     </div>
                   </div>
+
                   <!-- End username field-->
+
                     <!-- Start password field-->
+
                   <div class="form-group form-group-lg">
                     <label class="col-sm-2 control-label">Password</label>
                     <div class="col-sm-10 col-md-6">
@@ -61,40 +78,74 @@ if (isset($_SESSION['username'])){
                       <input type="password" name="password" class="form-control" autocomplete="new-password" />
                     </div>
                   </div>
+
                   <!-- End password field-->
+                  
                     <!-- Start email field-->
+
                   <div class="form-group form-group-lg">
                     <label class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10 col-md-6">
-                      <input type="email" name="email" class="form-control" />
+                      <input type="email" name="email" value="<?php echo $row['Email'] ?>" class="form-control" />
                     </div>
                   </div>
+
                   <!-- End email field-->
+                  
                     <!-- Start fullname field-->
+
                   <div class="form-group form-group-lg">
                     <label class="col-sm-2 control-label">Full Name</label>
                     <div class="col-sm-10 col-md-6">
-                      <input type="text" value="full name" class="form-control" />
+                      <input type="text" name="full"  value="<?php echo $row['FullName'] ?>" class="form-control" />
                     </div>
                   </div>
+
                   <!-- End fullname field-->
+                  
                     <!-- Start submit field-->
+
                   <div class="form-group form-group-lg">
                     <div class="col-sm-offset-2 col-sm-10">
                       <input type="submit" value="Save" class="btn btn-primary btn-lg" />
                     </div>
                   </div>
+
                   <!-- End submit field-->
+
                 </form>
             </div>
 
 
     <?php  
+         
+         // else show message
 
            } else {
 
             echo ' Theres  No ID';
            }
+     } elseif ($do == 'Update') { //  Update Page
+
+                 echo  "<h1 class='text-center'> Update Member </h1>";
+
+                 if ($_SERVER['REQUEST_METHOD']=='POST'){
+                  // Get varibles Form The Form 
+
+                  $id      =   $_POST['userid'];
+                  $user    =   $_POST['username'];
+                  $email   =   $_POST['email'];
+                  $name    =   $_POST['full'];
+
+                  echo $id . $user . $email . $name;
+
+
+                 }else{
+
+                   echo 'Sorry You Cant Browse This Page Directly';
+                 }
+
+       
      }
 
 
